114f79b02c3512364f5a7a987fd85b1b4ab68126
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2021-05-07 00:55:29 -0300

    Print kth element of a LinkedList using iterative algorithm that are going to count the elements in linked list and another one that is going to push two pointers with a gap of index required.
    So from now on the iterative solution is going to deliver O(n) for
    time complexity and O(1) for space complexity.

--- src/main/java/com/personal/rackingcode/LinkedList.java
+++ src/main/java/com/personal/rackingcode/LinkedList.java
@@ -16,21 +16,67 @@ public class LinkedList {
             .next(389)
             .next(42);
 
-        printKthElementList(head, 6);
+        int pos = 3;
+        printKthElementList_recursive(head, pos);
+        printKthElementList_iteratively(head, pos);
+        printKthElementList_iteratively_v2(head, pos);
+    }
+
+    //O(n) time complexity
+    //O(1) space complexity
+    static int printKthElementList_iteratively_v2(Node<Integer> head, int index) {
+      Node<Integer> p1 = head;
+
+      for (int i = 0; i < index; i++) {
+          p1 = p1.next;
+      }
+
+      Node<Integer> p2 = head;
+      while (p1 != null) {
+          p1 = p1.next;
+          p2 = p2.next;
+      }
+
+      System.out.println("node.value = " + p2.value);
+      return p2.value;
+    }
+
+    //O(n) time complexity
+    //O(1) space complexity
+    static int printKthElementList_iteratively(Node<Integer> head, int index) {
+        int p1 = 0;
+
+        Node<Integer> node = head;
+
+        while (node != null) {
+            node = node.next;
+            p1++;
+        }
+
+        int p2 = p1 - index;
+        node = head;
+
+        while (p2 > 0) {
+            node = node.next;
+            p2--;
+        }
+
+        System.out.println("node.value = " + node.value);
+        return node.value;
     }
 
     //O(n) in the worst case of time complexity
     //O(1) space complexity
-    static int printKthElementList(Node<Integer> head, int index) {
+    static int printKthElementList_recursive(Node<Integer> head, int index) {
         if (head == null) return 0;
-        
-        int pos = printKthElementList(head.next, index) + 1;
-        
+
+        int pos = printKthElementList_recursive(head.next, index) + 1;
+
         if (pos == index) System.out.println("head.value = " + head.value);
 
         return pos;
     }
-    
+
     //O(n) of time complexity and O(n of different values) space complexity
     static List<Integer> removeDuplicated_hashTable(java.util.LinkedList<Integer> in) {
         HashSet<Integer> hash = new HashSet<>();
