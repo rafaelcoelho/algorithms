71d90d63fe9bdf6d0f359fd72edd4f4126e20c4f
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2021-06-15 20:44:47 -0300

    Add more testing case for missing integer number

--- src/main/java/com/personal/leetcode/array/MissingPositive.java
+++ src/main/java/com/personal/leetcode/array/MissingPositive.java
@@ -3,6 +3,8 @@ package com.personal.leetcode.array;
 public class MissingPositive {
     public static void main(String[] args) {
         System.out.println("Missing value is: " + firstMissingPositive(new int[]{2147483647}));
+        System.out.println("Missing value is: " + firstMissingPositive(new int[]{0,10,2,1}));
+        System.out.println("Missing value is: " + firstMissingPositive(new int[]{20,19,18,-1,0,1,-0}));
     }
 
     public static int firstMissingPositive(int[] nums) {
