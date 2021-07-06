e5d3b36caddd8e7efe09b1dcde39e2a4e9af1604
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2021-07-05 23:05:39 -0300

    Adding base to run algorithm in order to figure out if a tree is a BST

---
+++ src/main/java/com/personal/leetcode/graph/BST.java
@@ -0,0 +1,53 @@
+package com.personal.leetcode.graph;
+
+import java.io.Serializable;
+
+public class BST {
+    static class Node <T> implements Serializable {
+        public T value;
+        public Node<T> rigth;
+        public Node<T> left;
+
+        public Node(T value) {
+            this.rigth = null;
+            this.left = null;
+            this.value = value;
+        }
+
+        public Node<T> left(T n) {
+            this.left = new Node<>(n);
+            return this.left;
+        }
+
+        public Node<T> rigth(T n) {
+            this.rigth = new Node<>(n);
+            return this;
+        }
+    }
+
+    public static void main(String[] args) {
+        var root = new Node<Integer>(10);
+
+        Node<Integer> left = new Node<>(5);
+
+        left.rigth(6)
+            .left(3)
+            .rigth(4)
+            .left(2);
+
+        root.left = left;
+
+        Node<Integer> rigth = new Node<>(15);
+
+        rigth.rigth(18)
+            .left(13)
+            .rigth(14)
+            .left(12);
+
+        rigth.rigth.rigth(25)
+            .left(20);
+
+        root.rigth = rigth;
+
+    }
+}
